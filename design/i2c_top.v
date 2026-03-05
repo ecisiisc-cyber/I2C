fd4e9b8bc36407c7c54d6086c1265e782d7186c9
Hariram-Lab <ecisiisc@gmail.com>
2026-03-05 16:00:06 +0530

    i2c top is created

---
+++ design/i2c_top.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ECIS,IISc
+// Engineer: Hariram P
+// 
+// Create Date: 03/05/2026 03:27:12 PM
+// Design Name: 
+// Module Name: i2c_top
+// Project Name: 
+// Target Devices: neso a7
+// Tool Versions: 2025.1
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module i2c_top(
+
+    );
+endmodule
